0a606a42720bf95dbfe122a89f5c7f3db82e4f1d
Gustavo Silva <gus7silva@berkeley.edu>
2016-08-06 21:20:01 -0700

    code formatting

--- app/src/main/java/com/pictureperfect/MainActivity.java
+++ app/src/main/java/com/pictureperfect/MainActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.pictureperfect;
 
 import android.Manifest;
@@ -70,12 +55,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
-//import com.google.android.gms.vision.CameraSource;
-
-/**
- * Activity for the face tracker app.  This app detects mFaces with the rear facing camera, and draws
- * overlay graphics to indicate the position, size, and ID of each face.
- */
 public final class MainActivity extends AppCompatActivity {
     private static final String TAG = "Perfect";
     private static final String IMAGE_DIRECTORY = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/Perfect/";
