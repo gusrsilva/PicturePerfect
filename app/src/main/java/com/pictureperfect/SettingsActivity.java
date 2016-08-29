2fda58670c9e99575e6142fe0eadb719e1fce74b
Gustavo Silva <gus7silva@berkeley.edu>
2016-08-28 20:09:14 -0700

    updated settings

--- app/src/main/java/com/pictureperfect/SettingsActivity.java
+++ app/src/main/java/com/pictureperfect/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.pictureperfect.ui.camera.SettingsFragment;
 
@@ -14,6 +15,7 @@ public class SettingsActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
         getFragmentManager()
                 .beginTransaction()
                 .replace(R.id.frag_holder, new SettingsFragment())
